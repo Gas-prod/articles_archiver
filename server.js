5320f2b4ed880103f1b69814444cd51cfc242d7c
Gaspard Bureau <gas.bureau@gmail.com>
2022-11-26 17:28:13 +0100

    first commit

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const app = express()
 
 app.use(express.json())
 
-
+app.get('/', (_, res) => {
+    res.send('coucou')
+})
 
 app.listen(PORT, () => {
     console.log(`Le serveur est lancé sur le port ${PORT}`)
