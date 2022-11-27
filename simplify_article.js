3b462140f5212273d48e32d50fd06c55bd3b71fe
Gaspard Bureau <gas.bureau@gmail.com>
2022-11-27 16:13:34 +0100

    commit

--- simplify_article.js
+++ simplify_article.js
@@ -4,20 +4,23 @@ const request = require('request')
 const fs = require('fs');
 const htmlMd = require('node-html-markdown')
 
-let req = request('https://www.cairn.info/revue-les-cahiers-du-numerique-2009-2-page-53.htm', function(error, response, body){
-    var doc = new JSDOM(body);
+let simplify = (url) => {
+    let req = request(url, function(error, _, body){
+        let doc = new JSDOM(body);
 
-    let reader = new Readability(doc.window.document);
-    let article = reader.parse();
-    
-    if (!error){
-        fs.writeFile('content.md', htmlMd.NodeHtmlMarkdown.translate(article.content), function (err) {  
-            if (err) throw err;
-            console.log('Fichier créé !');
-        });
-        console.log(article.title)
-    }
-    else {
-        console.log(error)
-    }
-})
+        let reader = new Readability(doc.window.document);
+        let article = reader.parse();
+        
+        if (!error){
+            fs.writeFile('test.md', htmlMd.NodeHtmlMarkdown.translate(article.content), function (err) {  
+                if (err) throw err;
+                console.log('Fichier créé !');
+            });
+        }
+        else {
+            console.log(error)
+        }
+    })
+}
+
+module.exports = simplify
